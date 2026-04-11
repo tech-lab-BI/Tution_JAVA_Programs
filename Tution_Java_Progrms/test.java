a7d5674574fd92e88fb3949bd45c0b76a92207bc
bikram <techmarketu9@gmail.com>
2026-04-11 21:12:56 +0530

    day4

---
+++ Tution_Java_Progrms/test.java
@@ -0,0 +1,16 @@
+package Tution_Java_Progrms;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a = 100;
+		Integer b = 100;
+		
+		// int x = 100;
+		// int y = 100;
+		
+		System.out.print(a == b);
+	}
+
+}
